852051d8a7e08ce21a73e027cbd3a23da75718a2
Chetan Bhat <35666818+chetansb77@users.noreply.github.com>
2023-04-16 22:07:05 +0530

    (fix) error test cases removed

--- src/components/utility/__test__/code.spec.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Code from '../Code';
-
-it('error component renders correctly', () => {
-  const tree = renderer.create(<Code codeSnippet="Error traceback" />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
